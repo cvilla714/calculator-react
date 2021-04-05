b08d5eba648bf6a2fd66dbeca805420cec92b60c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-05 19:22:13 -0400

    added the groups and applied map to render the buttons and apply classes

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,8 +1,43 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/no-this-in-sfc */
 /* eslint-disable quotes */
 import React from "react";
 
+const nums1 = ["AC", "+/-", "%", "÷"];
+const nums2 = [7, 8, 9, "*"];
+const nums3 = [4, 5, 6, "-"];
+const nums4 = [1, 2, 3, "+"];
+const nums5 = [0, ".", "="];
 function ButtonPanel() {
-  return <h1>should render all button components</h1>;
+  return (
+    <div className="group1">
+      {nums1.map((num) => (
+        <button className={`${num === "÷" && "orange"}`} key={num}>
+          {num}
+        </button>
+      ))}
+      {nums2.map((num) => (
+        <button className={`${num === "*" && "orange"}`} key={num}>
+          {num}
+        </button>
+      ))}
+      {nums3.map((num) => (
+        <button className={`${num === "-" && "orange"}`} key={num}>
+          {num}
+        </button>
+      ))}
+      {nums4.map((num) => (
+        <button className={`${num === "+" && "orange"}`} key={num}>
+          {num}
+        </button>
+      ))}
+      {nums5.map((num) => (
+        <button className={`${num === "=" && "orange"} ${num === 0 && "bigN"} `} key={num}>
+          {num}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default ButtonPanel;
