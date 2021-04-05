8fa1d85e21bb9aadd065e918b53f7842cb97ad61
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-05 16:49:08 -0400

    transformed app from class to functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-/* eslint-disable react/prefer-stateless-function */
-// eslint-disable-next-line quotes
 import React from "react";
+import Button from "./Button";
+import ButtonPanel from "./ButtonPanel.jsx";
+import Display from "./Display";
 
-class App extends React.Component {
-  render() {
-    return <h1 className="title">Project Calculator</h1>;
-  }
+function App() {
+  return;
+  <div>
+    <h1 className="title">Project Calculator</h1>;
+    <div className="calculator">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+    </div>
+  </div>;
 }
 export default App;
