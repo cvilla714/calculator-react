c697ade3907df0ed4d18e04cd5602d4152b2c924
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-06 13:40:55 -0400

    created the logic to apply the correct class and added PropType and default value to empty

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,24 @@
 /* eslint-disable quotes */
+
+import PropTypes from "prop-types";
 import React from "react";
 
-function Button() {
-  return <h1>should accept button as prop </h1>;
+function Button({ name }) {
+  const symbols = name === "÷" || name === "*" || name === "-" || name === "+" || name === "=" ? "orange" : "";
+  const bigN = name === 0 ? "bigN" : "";
+  return (
+    <button type="button" className={`${symbols} ${bigN}`}>
+      {name}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  name: PropTypes.string,
+};
+
+Button.defaultProps = {
+  name: "",
+};
 
 export default Button;
