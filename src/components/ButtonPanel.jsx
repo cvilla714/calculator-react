25b20e85742ba12260d3aded76d4bf1a53098fb4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-06 13:39:49 -0400

    Added the Button component and included the prop from the button panel

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -2,39 +2,31 @@
 /* eslint-disable react/no-this-in-sfc */
 /* eslint-disable quotes */
 import React from "react";
+import Button from "./Button";
 
 const nums1 = ["AC", "+/-", "%", "÷"];
 const nums2 = [7, 8, 9, "*"];
 const nums3 = [4, 5, 6, "-"];
 const nums4 = [1, 2, 3, "+"];
 const nums5 = [0, ".", "="];
+
 function ButtonPanel() {
   return (
     <div className="group1">
       {nums1.map((num) => (
-        <button className={`${num === "÷" && "orange"}`} key={num}>
-          {num}
-        </button>
+        <Button name={num} key={num} />
       ))}
       {nums2.map((num) => (
-        <button className={`${num === "*" && "orange"}`} key={num}>
-          {num}
-        </button>
+        <Button name={num} key={num} />
       ))}
       {nums3.map((num) => (
-        <button className={`${num === "-" && "orange"}`} key={num}>
-          {num}
-        </button>
+        <Button name={num} key={num} />
       ))}
       {nums4.map((num) => (
-        <button className={`${num === "+" && "orange"}`} key={num}>
-          {num}
-        </button>
+        <Button name={num} key={num} />
       ))}
       {nums5.map((num) => (
-        <button className={`${num === "=" && "orange"} ${num === 0 && "bigN"} `} key={num}>
-          {num}
-        </button>
+        <Button name={num} key={num} />
       ))}
     </div>
   );
