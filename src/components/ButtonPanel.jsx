54f63c007f630fc9abf6d1c7b621e3a11307f7e5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-07 15:50:51 -0400

    added the handleClick to all buttons

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
 const nums1 = ['AC', '+/-', '%', '÷'];
@@ -7,26 +8,30 @@ const nums3 = [4, 5, 6, '-'];
 const nums4 = [1, 2, 3, '+'];
 const nums5 = [0, '.', '='];
 
-function ButtonPanel() {
+function ButtonPanel({ clickHandler }) {
   return (
     <div className="group1">
       {nums1.map((num) => (
-        <Button name={num} key={num} />
+        <Button name={num} key={num} handleClick={clickHandler} />
       ))}
       {nums2.map((num) => (
-        <Button name={num} key={num} />
+        <Button name={num} key={num} handleClick={clickHandler} />
       ))}
       {nums3.map((num) => (
-        <Button name={num} key={num} />
+        <Button name={num} key={num} handleClick={clickHandler} />
       ))}
       {nums4.map((num) => (
-        <Button name={num} key={num} />
+        <Button name={num} key={num} handleClick={clickHandler} />
       ))}
       {nums5.map((num) => (
-        <Button name={num} key={num} />
+        <Button name={num} key={num} handleClick={clickHandler} />
       ))}
     </div>
   );
 }
 
 export default ButtonPanel;
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
