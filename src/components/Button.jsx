dd78846b82ad5ec8b1e653cb5c86568e987165ce
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-07 15:50:29 -0400

    added the onClick event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function Button({ name }) {
+function Button({ name, handleClick }) {
   const symbols = name === '÷' || name === '*' || name === '-' || name === '+' || name === '=' ? 'orange' : '';
   const bigN = name === 0 ? 'bigN' : '';
   return (
-    <button type="button" className={`${symbols} ${bigN}`}>
+    <button
+      type="button"
+      className={`${symbols} ${bigN}`}
+      onClick={() => {
+        handleClick(name);
+      }}
+    >
       {name}
     </button>
   );
 }
 
 Button.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
-Button.defaultProps = {
-  name: '',
-};
+// Button.defaultProps = {
+//   name: '',
+// };
 
 export default Button;
