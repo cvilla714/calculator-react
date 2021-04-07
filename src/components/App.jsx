a009fcb460545c369d961e19f7410d4cfdd4c6f8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-07 15:50:06 -0400

    changed to class compoenent added the constructor and super passed props as well and added the handleClick function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,40 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div>
-      <h1 className="title">Project Calculator</h1>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <div className="calculator">
-        <Display result={calculate} />
-        <ButtonPanel />
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(operation) {
+    this.setState((prevState) => calculate(prevState, operation));
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+
+    return (
+      <div>
+        <h1 className="title">Project Calculator</h1>
+
+        <div className="calculator">
+          <Display
+            result={`${total || ''}
+          ${operation || ''}
+          ${(total && next) || ''}
+          `}
+          />
+          <ButtonPanel clickHandler={this.handleClick} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default App;
