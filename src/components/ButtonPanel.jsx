feadfd2026506c618ebf4c3b731fdce3b2108c6e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-07 18:30:16 -0400

    changed from  numbers to string

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 const nums1 = ['AC', '+/-', '%', '÷'];
-const nums2 = [7, 8, 9, '*'];
-const nums3 = [4, 5, 6, '-'];
-const nums4 = [1, 2, 3, '+'];
-const nums5 = [0, '.', '='];
+const nums2 = ['7', '8', '9', '*'];
+const nums3 = ['4', '5', '6', '-'];
+const nums4 = ['1', '2', '3', '+'];
+const nums5 = ['0', '.', '='];
 
 function ButtonPanel({ clickHandler }) {
   return (
