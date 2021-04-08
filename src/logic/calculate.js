a22e777c710ceab10add3ea232ea9072b357ac58
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-07 22:12:39 -0400

    eliminated the symbol percentage from the symbol array and added the condition to make the evaluation

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,7 +4,7 @@ import operate from './operate';
 function calculate(data, buttonName) {
   let { total, next, operation } = data;
   let result;
-  const symbols = ['+/-', '%', '÷', '*', '-', '+'];
+  const symbols = ['÷', '*', '-', '+'];
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
   if (buttonName === '=') {
@@ -29,6 +29,15 @@ function calculate(data, buttonName) {
       operation: null,
     };
   }
+
+  if (buttonName === '%') {
+    result = {
+      total: total * (next / 100),
+      next: null,
+      operation: null,
+    };
+  }
+
   if (symbols.includes(buttonName)) {
     if (!next) {
       result = {
