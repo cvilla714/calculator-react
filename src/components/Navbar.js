3201eeba245a9d48295860c10a1c234a945b1a12
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-13 01:13:23 -0400

    added the links for each route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
 /* eslint-disable object-curly-newline */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg" className="top">
-      <Navbar.Brand href="#home" className="main-title">
+      <Navbar.Brand href="#home" className="main-title text-warning ml-5">
         Math Magicians
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggle" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Calculator</Nav.Link>
-          <Nav.Link href="#home">Quote</Nav.Link>
+          <Nav.Link href="">
+            <Link to="/">Home</Link>
+          </Nav.Link>
+          <Nav.Link href="#link">
+            {' '}
+            <Link to="/calculator">Calculator</Link>
+          </Nav.Link>
+          <Nav.Link href="#home">
+            <Link to="/quote">Quote</Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
